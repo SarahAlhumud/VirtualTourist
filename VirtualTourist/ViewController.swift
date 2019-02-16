ba350a1ba8fa1334a4cd7987a1c667810ed7bb3e
SarahAlhumud <sara.alhumud@gmail.com>
2019-02-16 06:44:30 +0300

    Initial Commit

---
+++ VirtualTourist/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VirtualTourist
+//
+//  Created by Sarah Alhumud on 11/06/1440 AH.
+//  Copyright © 1440 Sarah Alhumud. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
